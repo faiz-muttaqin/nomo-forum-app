9bef33d58206c4b5c32962781ba3601ef9e0025d
Faiz Muttaqin <38288096+faiz-muttaqin@users.noreply.github.com>
2025-09-02 21:14:57 +0700

    add deployment docs

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import pluginReact from 'eslint-plugin-react'
-import { defineConfig } from 'eslint/config'
-import daStyle from 'eslint-config-dicodingacademy'
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import daStyle from 'eslint-config-dicodingacademy';
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
