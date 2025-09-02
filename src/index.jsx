867517f631ab57e7ac7d3f01a7408f6c8963995d
Faiz Muttaqin <38288096+faiz-muttaqin@users.noreply.github.com>
2025-09-02 21:20:33 +0700

    fix doc path

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './styles/style.css';
 import store from './states';
@@ -9,10 +9,10 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <StrictMode>
         <App />
       </StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
